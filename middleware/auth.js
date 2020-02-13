f3a45079506f7b96b1f5bc4650a7509faac5a7bc
Naughty1905 <minion1905@gmail.com>
2020-02-13 14:53:29 +0300

    variables changes

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,20 +1,24 @@
 function cookiesCleaner(req, res, next) {
-    if (req.cookies.user_sid && !req.session.user) {
-      res.clearCookie("user_sid");
-    }
+  if (req.cookies.token && !req.session.user) {
+    res.clearCookie('user_sid');
+  }
+  next();
+}
+
+// middleware function to check for logged-in users
+const sessionChecker = (req, res, next) => {
+  if (req.session.user) {
+    res.redirect('/ourDbname');
+  } else {
     next();
   }
-  
-  // middleware function to check for logged-in users
-  const sessionChecker = (req, res, next) => {
-    if (req.session.user) {
-      res.redirect("/ourDbname");
-    } else {
-      next();
-    }
-  };
-  
-  module.exports = {
-    sessionChecker,
-    cookiesCleaner
-  };
+};
+
+const rolesChecker = (req, res, next) => {
+
+};
+
+module.exports = {
+  sessionChecker,
+  cookiesCleaner
+};
